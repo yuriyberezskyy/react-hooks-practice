7b26661788c596807e2f00cf29baeb14945be17b
yuriyberezskyy <bereza95@gmail.com>
2020-06-24 13:14:17 -0400

    added removeIngredientHandler

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -12,13 +12,19 @@ function Ingredients() {
       { id: Math.random().toString(), ...ingredient },
     ]);
   };
+
+  const removeIngredientHandler = (idIngredient) => {
+    const filteredIngredients = userIngredients.filter(el => el.id !== idIngredient)
+    setUserIngredients(filteredIngredients)
+  }
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );

648e3ccfd1de0508c2fbee1aa4e27af7bea37688
yuriyberezskyy <bereza95@gmail.com>
2020-06-25 10:46:00 -0400

    Added spinner when adding and removing ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -5,6 +5,7 @@ import Search from "./Search";
 import IngredientList from "./IngredientList";
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     console.log("Rerender:", userIngredients)
@@ -12,11 +13,13 @@ function Ingredients() {
 
 
   const addIngredientHandler = (ingredient) => {
+    setIsLoading(true)
     fetch('https://react-update-hooks-347e1.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     }).then(response => {
+      setIsLoading(false)
       return response.json();
     }).then(data => {
       setUserIngredients((prevIngredients) => [
@@ -27,9 +30,11 @@ function Ingredients() {
   };
 
   const removeIngredientHandler = (idIngredient) => {
+    setIsLoading(true)
     fetch(`https://react-update-hooks-347e1.firebaseio.com/ingredients/${idIngredient}.json`, {
       method: 'DELETE',
     }).then(response => {
+      setIsLoading(false)
       const filteredIngredients = userIngredients.filter(el => el.id !== idIngredient)
       setUserIngredients(filteredIngredients)
     })
@@ -43,7 +48,7 @@ function Ingredients() {
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
 
       <section>
         <Search onLoadingIngredients={filteredIngredientsHandler} />
